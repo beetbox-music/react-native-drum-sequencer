303a95531f71659d01906563f4e786221b2e15fe
Claudio <clode.paladini@gmail.com>
2023-03-04 10:26:45 +0000

    expo

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import { StatusBar } from 'expo-status-bar';
 
 import Styles from "./components/Styles";
 import DrumMachine from "./components/DrumMachine";
-import Header from "./components/Header";
 
 export default function App() {
-
+ 
   return (
     <View style={Styles.container}>
       <StatusBar style="light" />
